a31316fc3c20f3446f67db61cbde2fa2f2095e9a
Andrei Hansel <minecraftcaptain52@gmail.com>
2025-01-15 02:30:01 +0700

    fix: mobile screen responsive bug for bento grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
   return (
     <>
       <RetroGridDemo />
-      <div className="w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto items-center justify-items-center min-h-screen p-4 sm:p-8 pb-20 gap-16 md:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Intro />
         
-            <div className="w-full mt-5 mb-10 mx-auto">
-              <BentoDemo />
-            </div>
+          <div className="w-full mt-5 mb-10 mx-auto">
+            <BentoDemo />
+          </div>
          
-         <CustomDock />
+          <CustomDock />
         </main>
       </div>
     </>
